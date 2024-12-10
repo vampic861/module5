a2aa1f90d46a61cf888efe3225581099763b4683
vampic861 <125185219+vampic861@users.noreply.github.com>
2024-12-10 14:07:22 +1000

    Add files via upload

---
+++ modul5_2.py
@@ -0,0 +1,40 @@
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.number_of_floors = floors
+
+    def go_to(self, new_floor):
+        for i in range(1, new_floor + 1):
+            if 1 <= new_floor and new_floor <= self.number_of_floors:
+                print(i)
+            else:
+                print('Такого этажа не существует')
+                break
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+
+
+h1 = House('ЖК Горский', 18)
+
+h2 = House('Домик в деревне', 2)
+
+h1.go_to(5)
+
+h2.go_to(10)
+# __str__
+
+print(h1)
+
+print(h2)
+
+
+
+# __len__
+
+print(len(h1))
+
+print(len(h2))
+
